0c396958003c2f9c6694fe2750d1c3cd4d157e7c
Robert Fratila <fratilarobert@hotmail.com>
2017-04-28 11:15:56 -0400

    Reformat code

--- net.py
+++ net.py
@@ -1,33 +1,45 @@
 import theano
+import theano.tensor as T
 import lasagne
 import pandas as pd
 import numpy as np
 
-def create_network(dimensions, input_var):
-    #dimensions = (1,1,data.shape[0],data.shape[1]) #We have to specify the input size because of the dense layer
-    #We have to specify the input size because of the dense layer
+def create_dense_network(dimensions, input_var):
+    '''
+        Generates a fully connected layer
+        Args:
+            dimension: the size of the incoming theano tensor
+            input_var: a theano tensor representing your data input
+
+        Returns: the output of the network (linked up to all the layers)
+    '''
     print ("Creating Network...")
     network = lasagne.layers.InputLayer(shape=dimensions,input_var=input_var)
     print ('Input Layer:')
     print ' ',lasagne.layers.get_output_shape(network)
     print ('Hidden Layer:')
 
-    #extra layers if learning capacity is not reached. e.g the data is high-dimensional
-
-    network = lasagne.layers.DenseLayer(network, num_units=1024, nonlinearity=lasagne.nonlinearities.rectify)
+    network = lasagne.layers.DenseLayer(network, 
+                                        num_units=32000,
+                                        nonlinearity=lasagne.nonlinearities.rectify)
     network = lasagne.layers.DropoutLayer(network,p=0.5)
     print ' ',lasagne.layers.get_output_shape(network)
 
-    network = lasagne.layers.DenseLayer(network, num_units=2048, nonlinearity=lasagne.nonlinearities.rectify)
+    network = lasagne.layers.DenseLayer(network,
+                                        num_units=16000,
+                                        nonlinearity=lasagne.nonlinearities.rectify)
     network = lasagne.layers.DropoutLayer(network,p=0.5)
     print ' ',lasagne.layers.get_output_shape(network)
 
-    network = lasagne.layers.DenseLayer(network, num_units=2048, nonlinearity=lasagne.nonlinearities.rectify)
+    network = lasagne.layers.DenseLayer(network,
+                                        num_units=8000,
+                                        nonlinearity=lasagne.nonlinearities.rectify)
     network = lasagne.layers.DropoutLayer(network,p=0.5)
     print ' ',lasagne.layers.get_output_shape(network)
-    #extra layers if learning capacity is not reached. e.g the data is high-dimensional
 
-    network = lasagne.layers.DenseLayer(network, num_units=2, nonlinearity = lasagne.nonlinearities.softmax)
+    network = lasagne.layers.DenseLayer(network,    
+                                        num_units=2,
+                                        nonlinearity = lasagne.nonlinearities.softmax)
     print ('Output Layer:')
     print ' ',lasagne.layers.get_output_shape(network)
 
@@ -35,9 +47,9 @@ def create_network(dimensions, input_var):
 
 def main():
 
-    data = pd.read_csv('Citalopram_study.csv',low_memory='false',header = None,
+    data = pd.read_csv('data/Citalopram_study.csv',low_memory='false',header = None,
                       index_col=0)
-    #data = np.array(data)
+
     data = data.transpose()
     del data['Response']
     del data['Remission']
@@ -46,10 +58,14 @@ def main():
     del data['TimePoint']
     del data['(ng/ml/mg CIT dose)']
     del data['%improvement']
-    numPatients = np.count_nonzero(pd.unique(data.values[:,0]))
-    numAttributes = np.count_nonzero(pd.unique(data.values[0]))
+    num_patients = np.count_nonzero(pd.unique(data.values[:,0]))
+    num_attributes = np.count_nonzero(pd.unique(data.values[0]))
 
-
-    input_var = T.tensor4('input')
+    import pudb; pu.db
+    np_data = np.array(data)
+    input_var = T.dmatrix('input')
     y = T.dmatrix('truth')
-    network = create_network()
+    network = create_network(np_data.shape,input_var)
+
+if __name__ == "__main__":
+    main()
