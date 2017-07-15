1332400418ebec396e75b9b04e9042e4492b69f8
Robert Fratila <fratilarobert@hotmail.com>
2017-07-14 20:09:20 -0400

    Move tests to another file

--- src/net.py
+++ src/net.py
@@ -21,20 +21,16 @@ import theano.tensor as T
 
 from utils import get_class
 from utils import display_record
-from utils import get_confusion_matrix
-from utils import round_list
 from utils import get_timestamp
 
 from selu import selu
 from selu import AlphaDropoutLayer
 
-from scipy import integrate
+from model_tests import run_test
 
 import json
 
 import cPickle as pickle
-
-from sklearn import metrics
 
 sys.setrecursionlimit(5000)
 
@@ -93,7 +89,10 @@ class Network(object):
             [i for i in [self.input_var] if i],
             lasagne.layers.get_output(self.network, deterministic=True))
         self.record = None
-        self.timestamp = get_timestamp()
+        try:
+            self.timestamp
+        except AttributeError:
+            self.timestamp = get_timestamp()
 
     def create_dense_network(self, units, dropouts, nonlinearity):
         """
@@ -382,139 +381,13 @@ class Network(object):
             self.timestamp = get_timestamp()
 
     def conduct_test(self, test_x, test_y, figure_path='figures'):
-        """
-        Will conduct the test suite to determine model strength.
-
-        Args:
-            test_x: data the model has not yet seen to predict
-            test_y: corresponding truth vectors
-        """
-        if self.num_classes is None or self.num_classes == 0:
-            print ('Cannot conduct test: there\'s no classification layer')
-            return
-
-        if test_y.shape[1] > 1:
-            test_y = get_class(test_y)  # Y is in one hot representation
-
-        raw_prediction = self.forward_pass(input_data=test_x,
-                                           convert_to_class=False)
-
-        confusion_matrix = get_confusion_matrix(
-            prediction=get_class(raw_prediction),
-            truth=test_y
+        """Will conduct the test suite to determine model strength."""
+        run_test(
+            network=self,
+            test_x=test_x,
+            test_y=test_y,
+            figure_path=figure_path
         )
-
-        tp = np.diagonal(confusion_matrix).astype('float32')
-        tn = (np.array([np.sum(confusion_matrix)] *
-                       confusion_matrix.shape[0]) -
-              confusion_matrix.sum(axis=0) -
-              confusion_matrix.sum(axis=1) + tp).astype('float32')
-        # sum each column and remove diagonal
-        fp = (confusion_matrix.sum(axis=0) - tp).astype('float32')
-        # sum each row and remove diagonal
-        fn = (confusion_matrix.sum(axis=1) - tp).astype('float32')
-
-        sens = np.nan_to_num(tp / (tp + fn))  # recall
-        spec = np.nan_to_num(tn / (tn + fp))
-        sens_macro = np.nan_to_num(sum(tp) / (sum(tp) + sum(fn)))
-        spec_macro = np.nan_to_num(sum(tn) / (sum(tn) + sum(fp)))
-        dice = 2 * tp / (2 * tp + fp + fn)
-        ppv = np.nan_to_num(tp / (tp + fp))  # precision
-        ppv_macro = np.nan_to_num(sum(tp) / (sum(tp) + sum(fp)))
-        npv = np.nan_to_num(tn / (tn + fn))
-        npv_macro = np.nan_to_num(sum(tn) / (sum(tn) + sum(fn)))
-        accuracy = np.sum(tp) / np.sum(confusion_matrix)
-        f1 = np.nan_to_num(2 * (ppv * sens) / (ppv + sens))
-        f1_macro = np.average(np.nan_to_num(2 * sens * ppv / (sens + ppv)))
-
-        print ('{} test\'s results'.format(self.name))
-
-        print ('TP:'),
-        print (tp)
-        print ('FP:'),
-        print (fp)
-        print ('TN:'),
-        print (tn)
-        print ('FN:'),
-        print (fn)
-
-        print ('\nAccuracy: {}'.format(accuracy))
-
-        print ('Sensitivity:'),
-        print(round_list(sens, decimals=3))
-        print ('\tMacro Sensitivity: {:.4f}'.format(sens_macro))
-
-        print ('Specificity:'),
-        print(round_list(spec, decimals=3))
-        print ('\tMacro Specificity: {:.4f}'.format(spec_macro))
-
-        print ('DICE:'),
-        print(round_list(dice, decimals=3))
-        print ('\tAvg. DICE: {:.4f}'.format(np.average(dice)))
-
-        print ('Positive Predictive Value:'),
-        print(round_list(ppv, decimals=3))
-        print ('\tMacro Positive Predictive Value: {:.4f}'.format
-               (ppv_macro))
-
-        print ('Negative Predictive Value:'),
-        print(round_list(npv, decimals=3))
-        print ('\tMacro Negative Predictive Value: {:.4f}'.format
-               (npv_macro))
-
-        print ('f1-score:'),
-        print(round_list(f1, decimals=3))
-        print ('\tMacro f1-score: {:.4f}'.format(f1_macro))
-
-        if not os.path.exists(figure_path):
-            print ('Creating figures folder')
-            os.makedirs(figure_path)
-        print ('\tSaving figure to file: {}/{}{}_ROC.png'.format(
-            figure_path,
-            self.timestamp,
-            self.name)
-        )
-
-        if not os.path.exists('{}/{}{}'.format(figure_path, self.timestamp,
-                                               self.name)):
-            print ('Creating {}/{}{} folder'.format(figure_path,
-                                                    self.timestamp,
-                                                    self.name))
-            os.makedirs('{}/{}{}'.format(
-                figure_path,
-                self.timestamp,
-                self.name)
-            )
-        print ('\tSaving ROC figures to folder: {}{}'.format(
-            self.timestamp,
-            self.name)
-        )
-
-        for i in range(self.num_classes):
-
-            fpr, tpr, thresholds = metrics.roc_curve(test_y,
-                                                     raw_prediction[:, i],
-                                                     pos_label=i)
-
-            auc = integrate.trapz(tpr, fpr)
-            print ('AUC: {:.4f}'.format(auc))
-            print ('\tGenerating ROC_{} ...'.format(i))
-
-            plt.figure(2)
-            plt.ion()
-            plt.plot(fpr, tpr, label=("AUC: {:.4f}".format(auc)))
-            plt.title("ROC Curve for {}".format(self.name))
-            plt.xlabel('1 - specificity')
-            plt.ylabel('sensitivity')
-            plt.legend(loc='lower right')
-            plt.ylim(0.0, 1.0)
-            plt.xlim(0.0, 1.0)
-            plt.show()
-
-            plt.savefig('{}/{}{}/{}.png'.format(figure_path,
-                                                self.timestamp,
-                                                self.name, i))
-            plt.clf()
 
     def __getstate__(self):
         """Pickle save config."""
