03da0f19d3eded1a8f7bd1d1acf468369d1ab0c2
Matt Krause <mrkrause@gmail.com>
2017-10-31 10:36:59 -0400

    src/mnist_loader.py: Create validation set when loading data (optionally)
    Added two options: `validation_proportion` and `validation_seed` to load_mnist() and load_fashion_mnist().
    When None (the default), previous behavior is preserved (i.e., it returns only training and test sets)
    
    Added helper function _split_dataset()

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -4,7 +4,7 @@ import gzip
 import numpy as np
 
 
-def load_fashion_mnist():
+def load_fashion_mnist(validation_proportion=None, validation_seed=None):
     """
     Get the fashion MNIST training data (downloading it if it is not already accessible),
     and return it as NumPy arrays.
@@ -35,18 +35,24 @@ def load_fashion_mnist():
         print("No fashion MNIST test (10k) images found--downloading")
         _download_file("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz", 'data/fashion')
     print ('Loading testing images...')
-    t10k_images = _load_image("data/fashion/t10k-images-idx3-ubyte.gz")
+    test_images = _load_image("data/fashion/t10k-images-idx3-ubyte.gz")
 
     if not os.path.exists("data/fashion/t10k-labels-idx1-ubyte.gz"):
         print("No fashion MNIST test (10k) labels found--downloading")
         _download_file("http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz", 'data/fashion')
     print ('Loading testing labels...')
-    t10k_labels = _load_label('data/fashion/t10k-labels-idx1-ubyte.gz')
+    test_labels = _load_label('data/fashion/t10k-labels-idx1-ubyte.gz')
 
-    return train_images, train_labels, t10k_images, t10k_labels
+    if validation_proportion is None or validation_proportion == 0:
+        return train_images, train_labels, test_images, test_labels
+    else:
+        (train_images, train_labels, val_images, val_labels) = _split_dataset(train_images, train_labels,
+                                                                              val_prop=validation_proportion,
+                                                                              seed=validation_seed)
+        return train_images, train_labels, val_images, val_labels, test_images, test_labels
 
 
-def load_mnist():
+def load_mnist(validation_proportion=None, validation_seed=None):
     """
     Get the MNIST training data (downloading it if it is not already accessible),
     and return it as NumPy arrays
@@ -75,15 +81,21 @@ def load_mnist():
         print("No MNIST test (10k) images found--downloading")
         _download_file("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz")
     print ('Loading testing images...')
-    t10k_images = _load_image("data/t10k-images-idx3-ubyte.gz")
+    test_images = _load_image("data/t10k-images-idx3-ubyte.gz")
 
     if not os.path.exists("data/t10k-labels-idx1-ubyte.gz"):
         print("No MNIST test (10k) labels found--downloading")
         _download_file("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz")
     print ('Loading testing labels...')
-    t10k_labels = _load_label('data/t10k-labels-idx1-ubyte.gz')
+    test_labels = _load_label('data/t10k-labels-idx1-ubyte.gz')
 
-    return train_images, train_labels, t10k_images, t10k_labels
+    if validation_proportion is None or validation_proportion == 0:
+        return train_images, train_labels, test_images, test_labels
+    else:
+        (train_images, train_labels, val_images, val_labels) = _split_dataset(train_images, train_labels,
+                                                                              val_prop=validation_proportion,
+                                                                              seed=validation_seed)
+        return train_images, train_labels, val_images, val_labels, test_images, test_labels
 
 
 def _download_file(file_path, folder='data'):
@@ -112,9 +124,32 @@ def _load_label(filename):
     return data
 
 
+def _split_dataset(data, labels, val_prop, seed=None):
+    """Split a data set in two (i.e., train and validation)
+    Args:
+        data:  Data set (examples X dims)
+        labels:  Corresponding class labels (examples X 1)
+        val_prop: Proportion of examples reserved for the validation set (0-1)
+        seed: If not None, this int/array will be used to seed the RNG that selects the samples
+    Returns:
+        (train_data, train_labels, val_data, val_labels)
+
+    Note: The data in each new set is a copy of the original, not a view
+
+    """
+    rng = np.random.RandomState(seed)
+    ix = rng.permutation(data.shape[0])
+
+    n_val = np.floor(data.shape[0] * val_prop).astype('int')
+    return data[ix[n_val:], :], labels[ix[n_val:]].copy(), data[ix[:n_val], :].copy(), labels[ix[:n_val]].copy(),
+
+
+
+
 def main():
     """Totally useless main."""
     (train_iamges, train_labels, t10k_images, t10k_labels) = load_mnist()
+    (train_iamges, train_labels, val_images, val_labels, test_imgages, test_labels) = load_mnist(validation_proportion=0.1)
 
 if __name__ == "__main__":
     main()
