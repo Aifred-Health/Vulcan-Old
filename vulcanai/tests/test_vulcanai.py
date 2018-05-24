2427d5548f1b8aba15f558e4f24547fa87d1b465
Caitrin Armstrong <caitrin.armstrong@mail.mcgill.ca>
2018-05-23 20:02:31 -0400

    added first skeleton structure for writing and running tests

---
+++ vulcanai/tests/test_vulcanai.py
@@ -0,0 +1,13 @@
+""" Contains the primary test cases for the module """
+from unittest import TestCase
+
+import vulcanai
+
+#class TestSomething(TestCase):
+
+    #example form
+
+    #def test_is_string(self):
+    #    s = funniest.joke()
+    #    self.assertTrue(isinstance(s, basestring))
+
